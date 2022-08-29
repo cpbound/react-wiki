ccb7625f6da33bb1881156c4af7c4a19f77795f3
Christian Bound <automaticbound@gmail.com>
2022-08-29 12:47:52 +0900

    app.js api call working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
+import Search from "./components/Search/Search";
+import Card from "./components/Card/Card";
+import Pagination from "./components/Pagination/Pagination";
+import Filter from "./components/Filter/Filter";
+import Navbar from "./components/Navbar/Navbar";
 import React, { useState, useEffect } from "react";
-
 import './App.css';
 
+
 function App() {
+
+  let api = `https://ghibliapi.herokuapp.com/films`
+
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then((res) => res.json());
+      updateFetchedData(data);
+    })();
+  }, [api])
+
+  let [fetchedData, updateFetchedData] = useState([]);
+  let { info, results } = fetchedData;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-center mb-3">Characters</h1>
+      <div className="container">
+        <div className="row">
+          Filter component will be placed here
+          <div className="col-lg-8 col-12">
+            <div className="row">
+              <Card results={results} />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Card/Card.js
@@ -0,0 +1,12 @@
+
+const Card = ({ results }) => {
+
+  let display;
+
+  if (results) {}
+  else{
+    display = "No Characters Found 😰"
+  }
+
+  return <>{display}</>
+}
