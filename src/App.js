e942c5e47b49b50c7a5dc3773d05789d8df70659
Christian Bound <automaticbound@gmail.com>
2022-08-29 12:05:27 +0900

    init react commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import React, { useState, useEffect } from "react";
+
 import './App.css';
 
 function App() {
