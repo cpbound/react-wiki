f3fe1a6692a22b64b04b3c3cbfda13832e450790
Christian Bound <automaticbound@gmail.com>
2022-08-29 14:08:22 +0900

    filter functionality and filterBTN done

--- src/App.js
+++ src/App.js
@@ -11,9 +11,13 @@ import './App.css';
 
 function App() {
 
+  let [status, updateStatus] = useState("");
+  let [gender, updateGender] = useState("");
+  let [species, updateSpecies] = useState("");
   let [pageNumber, updatePageNumber] = useState(1);
   let [search, setSearch] = useState("");
-  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}`;
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}&status=${status}&gender=${gender}&species=${species}`;
+
 
   useEffect(() => {
     (async function () {
@@ -31,7 +35,15 @@ function App() {
       <Search setSearch={setSearch} updatePageNumber={updatePageNumber} />
       <div className="container">
         <div className="row">
-          Filter component will be placed here
+          <Filter
+            pageNumber={pageNumber}
+            status={status}
+            updateStatus={updateStatus}
+            updateGender={updateGender}
+            updateSpecies={updateSpecies}
+            updatePageNumber={updatePageNumber}
+          />
+
           <div className="col-lg-8 col-12">
             <div className="row">
               <Card results={results} />
